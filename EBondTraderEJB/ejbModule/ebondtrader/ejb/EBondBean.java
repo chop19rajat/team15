dd960d70ce367208d6c3abbf3353c086d358a3ef
ashwinrcs <ashwinrcs@gmail.com>
2016-08-30 18:39:08 +0530

    5 category custom filter
    5 category custom filter

--- EBondTraderEJB/ejbModule/ebondtrader/ejb/EBondBean.java
+++ EBondTraderEJB/ejbModule/ebondtrader/ejb/EBondBean.java
@@ -1,6 +1,6 @@
 package ebondtrader.ejb;
 
-import java.util.Arrays;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 import javax.ejb.Local;
@@ -9,9 +9,9 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.ws.rs.QueryParam;
 
-import ebondtrader.jpa.Categories;
-import ebondtrader.jpa.Products;
+import ebondtrader.jpa.Bond;
 
 /**
  * Session Bean implementation class EBondBean
@@ -20,118 +20,93 @@ import ebondtrader.jpa.Products;
 @Local(EBondBeanLocal.class)
 @Stateless
 public class EBondBean implements EBondBeanRemote, EBondBeanLocal {
-	
-	@PersistenceContext(unitName="EBondTraderJPA-PU")
+
+	@PersistenceContext(unitName = "EBondTraderJPA-PU")
 	EntityManager em;
-    /**
-     * Default constructor. 
-     */
-	public void addCategoriesAndProducts(){
-		
-		
-		//EntityManagerFactory emfactory=Persistence.createEntityManagerFactory("StudentOnlineRetailerJPA");
-		//EntityManager entitymanager=emfactory.createEntityManager();
-		//entitymanager.getTransaction().begin();
-		System.out.println("adding objects");
-		Categories category=new Categories();
-		Products product1 = new Products();
-		Products product2 = new Products();
-		//category.setCategoryId(1);
-		category.setCategoryName("Beverage");
-		category.setDescription("Soft Drinks");
-		
-		//product1.setProductID(111);
-		product1.setProductName("CocaCola");
-		product1.setUnitPrice(50);
-		product1.setUnitsInStock(100);
-		product1.setReorderLevel(2);
-		
-		//product2.setProductID(112);
-		product2.setProductName("Pepsi");
-		product2.setUnitPrice(50);
-		product2.setUnitsInStock(100);
-		product2.setReorderLevel(2);
-		
-		product1.setCategory(category);
-		product2.setCategory(category);
-		
-		List<Products> p1=Arrays.asList(product1,product2);
-		category.setList(p1);
-		//entitymanager.persist(category);
-		//entitymanager.persist(product1);
-		//entitymanager.persist(product2);
-		em.persist(category);
-		em.persist(product1);
-		em.persist(product2);
-		//entitymanager.getTransaction().commit();
-		
-	//	entitymanager.close();
-		//emfactory.close();
-						
+
+	/**
+	 * Default constructor.
+	 */
+
+	public List<Bond> getAllBonds() {
+		// Query query = em.createQuery("Select eb from " +
+		// Bond.class.getClass() + " eb");
+		System.out.println("in getAllBonds()!");
+		TypedQuery<Bond> query = em.createQuery("SELECT eb FROM Bond AS eb", Bond.class);
+		System.out.println("created query!");
+		List<Bond> bonds = (List<Bond>)query.getResultList();
+		System.out.println("got result list!");
+		return bonds;
 	}
-	
-	public void printing(){
+
+	public void printing() {
 		System.out.println("In Bean");
 	}
-	
-	
-	public List<Products> getProducts(){
-		
-		TypedQuery <Products>query=em.createQuery("Select p from Products as p",Products.class);
-		List<Products> plist=(List<Products>) query.getResultList();
-		for(Products p: plist){
-			System.out.println(p.getProductName());
-		}
-		
-		return plist;
-		
+
+	public List<Bond> getBondByIsin(String isin) {
+
+		String sql = "SELECT eb FROM Bond AS eb WHERE eb.isin = '" + isin + "'";
+		TypedQuery<Bond> myquery = em.createQuery(sql, Bond.class);
+		List<Bond> bond = (List<Bond>) myquery.getResultList();
+		return bond;
+
+	}
+
+	@Override
+	public List<Bond> getBondByCouponPeriod(String coupon_Period) {
+		String sql = "SELECT eb FROM Bond AS eb WHERE eb.coupon_Period = '" + coupon_Period + "'";
+		System.out.println("in getBondByCoupon");
+		TypedQuery<Bond> myquery = em.createQuery(sql, Bond.class);
+		List<Bond> bond = (List<Bond>) myquery.getResultList();
+		return bond;
+	}
+	@Override
+	public List<Bond> getBondByCouponPeriodAndFitch(String coupon_Period,String fitch ) {
+		String sql = "SELECT eb FROM Bond AS eb WHERE eb.coupon_Period = '" + coupon_Period + "'" + "AND eb.fitch LIKE '%" + fitch +"'";
+		System.out.println("in getBondByCoupon");
+		TypedQuery<Bond> myquery = em.createQuery(sql, Bond.class);
+		List<Bond> bond = (List<Bond>) myquery.getResultList();
+		return bond;
 	}
 	
-	
-	
-	public List<Products> getProductsByName(String name){
-		
-		/*Query query=em.createQuery("Select p from Products as p where p.productName="+name);
-		Products p=(Products) query.getSingleResult();
-		return p;*/
-		
-		String sql = "SELECT p FROM Products AS p WHERE p.productName LIKE '%" + name + "%'";
-        System.out.println(sql);
-        TypedQuery<Products> query = em.createQuery(sql, Products.class);
-        List<Products> prod = (List<Products>) query.getResultList();
-        for(Products p2:prod){
-        	System.out.println(p2.getProductID());
-        }
-        return  prod;
-		
+	@Override
+	public List<Bond> getBondByFitch(String fitch) {
+		String sql = "SELECT eb FROM Bond AS eb WHERE eb.fitch LIKE ' %" + fitch + "'";
+		System.out.println("in getBondByCoupon");
+		TypedQuery<Bond> myquery = em.createQuery(sql, Bond.class);
+		List<Bond> bond = (List<Bond>) myquery.getResultList();
+		return bond;
+	}
+
+	@Override
+	public List<Bond> getBondByIssuerName(String issuerName) {
+		String sql = "SELECT eb FROM Bond AS eb WHERE eb.issuerName LIKE '%" + issuerName + "%'";
+		System.out.println("in getBondByCoupon");
+		TypedQuery<Bond> myquery = em.createQuery(sql, Bond.class);
+		List<Bond> bond = (List<Bond>) myquery.getResultList();
+		return bond;
+	}
+
+//	@Override
+//	public List<Bond> getBondByCouponPeriodAndFitchRevised(String isin, String coupon_Period, String fitch) {
+//		String sql = "SELECT eb FROM Bond AS eb WHERE eb.coupon_Period like '" + coupon_Period + "'" + "AND eb.fitch LIKE '%" + fitch +"%'" + "AND eb.isin LIKE '%" + isin+"%'";
+//		System.out.println("in Revised method.");
+//		TypedQuery<Bond> myquery = em.createQuery(sql, Bond.class);
+//		List<Bond> bond = (List<Bond>) myquery.getResultList();
+//		return bond;
+//	}
+	@Override
+	public List<Bond> getBondByCouponPeriodAndFitchRevised(String isin, String coupon_Period, String fitch,String moodys,String snp) {
+		String sql = "SELECT eb FROM Bond AS eb WHERE eb.coupon_Period like '" + coupon_Period + "'" + "AND eb.fitch LIKE '%" + fitch +"%'" + "AND eb.isin LIKE '%" + isin+"%'"+"AND eb.moodys LIKE '" + moodys +"'" + "AND eb.snP LIKE '%" + snp+"%'"  ;
+		System.out.println("in Revised method.");
+		TypedQuery<Bond> myquery = em.createQuery(sql, Bond.class);
+		List<Bond> bond = (List<Bond>) myquery.getResultList();
+		return bond;
 	}
 	
-	/*public List<Products> getProductsInCategory(Categories category){
-		Query query=em.createQuery("Select p from Products as p where p.category ="+category);
-		List<Products> plist=(List<Products>) query.getResultList();
-		return plist;
-		
-	}*/
-	public List<Products> getProductsInCategory(String categoryName) {
 
-        TypedQuery<Products> query = em.createQuery("SELECT p FROM Products AS p WHERE p.category.categoryName = :name", Products.class);
-        query.setParameter("name", categoryName);
-
-        // Execute the query, and get a collection of products back.
-        List<Products> products = query.getResultList();
-
-        for (Products product: products) {
-            System.out.println(product.getProductName());;
-        }
-
-        return products;
-    }
-
-    /**
-     * Default constructor. 
-     */
-    public EBondBean() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
 
 }
--- EBondTraderWeb/src/mypackage/ProductResource.java
+++ EBondTraderWeb/src/mypackage/ProductResource.java
@@ -17,102 +17,92 @@ import javax.ws.rs.QueryParam;
 
 import ebondtrader.ejb.EBondBeanLocal;
 import ebondtrader.jpa.Bond;
-import ebondtrader.jpa.Products;
-
-
 
 @Path("/product")
 public class ProductResource {
-	
-	
-	Context context=null;
+
+	Context context = null;
 	EBondBeanLocal bean;
-	
-	ProductResource(){
-		try{
-			context=new InitialContext();
-			bean=(EBondBeanLocal)context.lookup("java:app/EBondTraderEJB/EBondBean!ebondtrader.ejb.EBondBeanLocal");
-		}
-		catch(Exception e){
+
+	ProductResource() {
+		try {
+			context = new InitialContext();
+			bean = (EBondBeanLocal) context.lookup("java:app/EBondTraderEJB/EBondBean!ebondtrader.ejb.EBondBeanLocal");
+			System.out.println("bean created!");
+		} catch (Exception e) {
 			System.out.println("Error in creating bean");
 			e.printStackTrace();
 		}
-		
-	}
-	
-	
-	
-//	@GET
-//	@Produces("application/json")
-//	public List<Products> retProducts{
-//		List<Products> plist=bean.getProducts();
-//		return plist;
-//	}
-	
-	/*@GET
-	@Produces("application/json")
-	public List<Products> getProductAsString(){
-		
-		if(bean==null)
-			return null;
-		List<Products> prod = bean.getProducts();
-		return prod;
-		
-	}*/
 
-	
-	@GET
-	@Produces("application/json")
-	public List<Products> getProductByName(@QueryParam("filter")@DefaultValue("")String filter){
-		
-		if(bean==null)
-			return null;
-		List<Products> prod = bean.getProductsByName(filter);
-		return prod;
-		
 	}
-	
-	
+
+	// @GET
+	// @Produces("application/json")
+	// public List<Products> retProducts{
+	// List<Products> plist=bean.getProducts();
+	// return plist;
+	// }
+
+	/*
+	 * @GET
+	 * 
+	 * @Produces("application/json") public List<Products> getProductAsString(){
+	 * 
+	 * if(bean==null) return null; List<Products> prod = bean.getProducts();
+	 * return prod;
+	 * 
+	 * }
+	 */
+
+//	s
+
 	@POST
 	@Consumes("text/plain")
-	@Path("/post")
-	public void ret(String text){
+	public void ret(String text) {
 		System.out.println(text);
-		System.out.println("qqqq");
-		System.out.println("qaaq");
 	}
-	
-	
-	/*@GET
-	@Produces("application/json")
-	@Path("/{categoryName}")
-	public List<Products> getProductsInCategory(@PathParam("categoryName") String categoryname){
-		
-		if(bean==null)
-			return null;
-		List<Products> prod = bean.getProductsInCategory(categoryname);
-		return prod;
-		
-	}*/
+
+	/*
+	 * @GET
+	 * 
+	 * @Produces("application/json")
+	 * 
+	 * @Path("/{categoryName}") public List<Products>
+	 * getProductsInCategory(@PathParam("categoryName") String categoryname){
+	 * 
+	 * if(bean==null) return null; List<Products> prod =
+	 * bean.getProductsInCategory(categoryname); return prod;
+	 * 
+	 * }
+	 */
+
+	// @GET
+	// @Produces("application/json")
+	// @Path("/{bonds}/{isin}")
+	// public List<Bond> getBond(@PathParam("bonds") String
+	// bonds,@PathParam("isin") String isin){
+	// return bean.getBondByIsin(isin);
+	//
+	// }
+
+	/*
+	 * @POST
+	 * 
+	 * @Consumes("text/plain")
+	 * 
+	 * @Produces("text/plain") public void putText(String content){
+	 * System.out.println(content); System.out.println("hELLO"); }
+	 */
 	
 	@GET
-	@Produces("text/plain")
-	@Path("/{name}/{age}")
-	public String retName(@PathParam("name") String name,@PathParam("age") String age){
-		System.out.println("Hello");
-		return name+age;
+	@Produces("application/json")
+	@Path("/allBondsByCustomSearch")
+	public List<Bond> getBond(@QueryParam("isin") @DefaultValue("%") String isin, @QueryParam("coupon_Period") @DefaultValue("%") String coupon_Period,
+			@QueryParam("fitch") @DefaultValue("%") String fitch,@QueryParam("moodys") @DefaultValue("%") String moodys,@QueryParam("snp") @DefaultValue("%") String snp) {
+		if(isin.equals("%") && coupon_Period.equals("%") && fitch.equals("%") && moodys.equals("%") && snp.equals("%")){
+			return bean.getAllBonds();
+		}else return bean.getBondByCouponPeriodAndFitchRevised(isin, coupon_Period, fitch,moodys,snp); 
+		}
 		
-	}
 	
-	
-	/*@POST
-	@Consumes("text/plain")
-	@Produces("text/plain")
-	public void putText(String content){
-		System.out.println(content);
-		System.out.println("hELLO");
-	}
-	*/
-	
-
 }
